cbeaaa04a668a348374a10cafe9934f753cdbb98
BramSwaanen <117469447+BramSwaanen@users.noreply.github.com>
2023-12-22 13:31:23 +0100

    Update data_transformations.py

--- data_transformations.py
+++ data_transformations.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def prepare_for_translation(dataset, columns: list):
   '''
   Prepares dataset for translation with simple_generate by 
@@ -26,5 +28,6 @@ def generate_samples_dict(prepared_dataset, n, generator):
   Generates a dictionary with as keys n samples selected from
   the prepared_dataset with as value their translation using generator.
   '''
-  data = np.random.shuffle(prepared_dataset)[:n]
+  np.random.shuffle(prepared_dataset)
+  data = prepared_dataset[:n]
   return generate_translation_dict(data,generator)
