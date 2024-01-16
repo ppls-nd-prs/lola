0f2edecf77563a7769c8800f95f3ed1314d413a2
BramSwaanen <117469447+BramSwaanen@users.noreply.github.com>
2024-01-16 16:42:04 +0100

    Updated preprocessing and testing
    Testing now finished,
    preprocessing improved.

--- prepocessing.py
+++ prepocessing.py
@@ -4,6 +4,8 @@ class preprocessing:
     
     def fol2nltk(s:str) -> str:
         s = re.sub('∧', '&', s)
+        s = re.sub('∨', '|', s)
+        s = re.sub('-', '_', s)
         s = re.sub('∃(\S\d*)', 'exists \\1.', s) #counts all chars after quantifier until there is white space as the var  
         s = re.sub('∀(\S\d*)', 'all \\1.', s)
         s = re.sub('→', '->', s)
--- testing.py
+++ testing.py
@@ -4,6 +4,8 @@ from prepocessing import preprocessing
 from datasets import load_dataset, concatenate_datasets
 import json
 import nltk
+import pandas as pd
+import re
 
 #locate prover9
 PROVER9_BIN = "./prover9/bin"
@@ -12,8 +14,6 @@ print(prover9_prove(PROVER9_BIN, "some y. man(y)", ["man(Alex)"]))
 #load fracas
 fracas = load_dataset("pietrolesci/fracas")
 fracas_dataset = concatenate_datasets([fracas["train"],fracas["dev"]])
-
-print(fracas_dataset)
 
 #get the dictionary
 with open("dictionaries/fracas/full_fracas_dict.json","r") as file:
@@ -55,48 +55,45 @@ with open("dictionaries/fracas/full_fracas_dict.json","r") as file:
 def negated(fol_string):
     return "not " + fol_string
 
-print(negated("all x. man(x)"))
-
 def evaluate(translation_dict,dataset,columns,judgment_dict):
     '''
     columns should have format [premise-column-name,hypothesis-column-name,
     label-column-name]
-    judgment_dict should have format {[value]:"entailment", [value]:"neutral",
-    [value]:"contradiction"}
+    judgment_dict should have format {[value]:"e", [value]:"n",
+    [value]:"c"}.
+    Returns a pandas dataframe with columns p_nl, h_nl, p_fol, h_fol, label, e_pred, c_pred
     '''
-    #TODO: i) test for contradiction by checking for entailment of not(hypothesis),
-    # ii) 
-    #0: entailment; 1: neutral; 2: contradiction
-    ee = 0; en = 0; ec = 0; ne = 0; nn = 0; nc = 0; ce = 0; cn = 0; cc = 0
-    syntactically_wrong = 0
+    df = pd.DataFrame({'p_nl':[],'h_nl':[],'p_fol':[],'h_fol':[],'label':[],'e_pred':[],
+                       'c_pred':[]})
+    e_df = pd.DataFrame({'nl_p':[],'nl_h':[],'exception':[]})
     for datapoint in dataset:
-        print('***')
-        print(datapoint)
-        print(datapoint[columns[0]])
-        print("***")
-        premise = datapoint[columns[0]]
-        hypothesis = datapoint[columns[1]]
+        dat_temp_dict = {}
+        e_temp_dict = {}
+        # get premise and hypothesis natural string representation
+        nl_premise = datapoint[columns[0]]
+        dat_temp_dict['p_nl'] = nl_premise
+        nl_hypothesis = datapoint[columns[1]]
+        dat_temp_dict['h_nl'] = nl_hypothesis
         true_label = judgment_dict[datapoint[columns[2]]]
-        print(true_label)
-        fol_premise = preprocessing.fol2nltk(translation_dict[premise])
-        print("not fol_premise: ", negated(fol_premise))
-        fol_hypothesis = preprocessing.fol2nltk(translation_dict[hypothesis])
+        dat_temp_dict['label'] = true_label
+
+        #Get nl_premise, hypothesis and not(hypothesis) fol string representation
+        fol_premise = translation_dict[nl_premise]
+        fol_premise = preprocessing.fol2nltk(fol_premise)
+        dat_temp_dict['p_fol'] = fol_premise
+        fol_hypothesis = translation_dict[nl_hypothesis]
+        fol_hypothesis = preprocessing.fol2nltk(fol_hypothesis)
+        fol_not_hypothesis = negated(fol_hypothesis)
+        dat_temp_dict['h_fol'] = fol_hypothesis
         try:
-            direct_pred_label = prover9_prove(PROVER9_BIN, fol_premise, [fol_hypothesis])
-        except:
-            syntactically_wrong += 1
-        if true_label == "entailment":
-            if pred_label == True:
-                ee += 1
-        elif label == True and (label == 1 or label == 2):
-            fp += 1
-        elif label == False and (label == 0):
-            fn += 1
-        elif label == False and (label == 1 or label == 2):
-            tn += 1
-        
-    return {"tp":tp,"fp":fp,"tn":tn,"fn":fn,"sw":syntactically_wrong}
+            dat_temp_dict['e_pred'] = prover9_prove(PROVER9_BIN, fol_hypothesis, [fol_premise])
+            dat_temp_dict['c_pred'] = prover9_prove(PROVER9_BIN, fol_not_hypothesis, [fol_premise])
+            df.loc[len(df)] = dat_temp_dict
+        except Exception as a:
+            e_df.loc[len(e_df)] = {'nl_p':nl_premise,'nl_h':nl_hypothesis,'exception':str(a)}
+    return df, e_df
 
-
-
-# print(evaluate(fracas_dict,fracas_dataset,['premise','hypothesis','label'],{0:"entailment",1:"neutral",2:"contradiction"}))
+df, e_df = evaluate(fracas_dict,fracas_dataset,['premise','hypothesis','label'],{0:"e",1:"n",2:"c"})
+print(df.head())
+df.to_csv("evaluation.csv")
+e_df.to_csv("exceptions.csv")
