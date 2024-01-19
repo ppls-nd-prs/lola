d4962ff0a4a490589915895090876e0646c116e7
jips99 <jipschoneveld@gmail.com>
2024-01-19 14:22:59 +0100

    collect all fol expressions

--- evaluation.py
+++ evaluation.py
@@ -1,4 +1,5 @@
 import pandas as pd 
+from nltk.sem.logic import Expression
 
 def get_wrong_items(results : pd.DataFrame) -> (pd.DataFrame, pd.DataFrame, pd.DataFrame) :
     wrong_e = "label == 'e' & (e_pred == False | c_pred == True)"
@@ -9,3 +10,13 @@ def get_wrong_items(results : pd.DataFrame) -> (pd.DataFrame, pd.DataFrame, pd.D
     wrong_items_n = results.query(wrong_n)
 
     return wrong_items_e, wrong_items_c, wrong_items_n 
+
+def get_fol_expressions(df : pd.DataFrame) -> [str]:
+    """
+    data: a dataframe with e.g. columns 'p_fol' and 'h_fol" 
+    returns: list of all fol formulas in p_fol and c_fol (as strings)
+    """
+    prem_fols = list(df['p_fol'])
+    hyp_fols = list(df['h_fol'])
+
+    return prem_fols + hyp_fols
