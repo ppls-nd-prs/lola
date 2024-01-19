64a4296c5f63d82bcdc766f98f01d601d5a1996a
jips99 <jipschoneveld@gmail.com>
2024-01-19 12:45:27 +0100

    get wrong items

--- evaluation.py
+++ evaluation.py
@@ -1,9 +1,11 @@
-import nltk 
+import pandas as pd 
 
-def proof_9(conclusion : nltk.sem.Expression, premises :[nltk.sem.Expression]):
-    prover9 = nltk.Prover9()
-    prover9.config_prover9("./prover9/bin")
-    try:
-        return prover9.prove(conclusion, premises) 
-    except OSError as e:
-        raise OSError(f"{e}. Try running this in Google Colab") from None #prover9 exec needs linux 
+def get_wrong_items(results : pd.DataFrame) -> (pd.DataFrame, pd.DataFrame, pd.DataFrame) :
+    wrong_e = "label == 'e' & (e_pred == False | c_pred == True)"
+    wrong_c = "label == 'c' & (c_pred == False | e_pred == True)"
+    wrong_n = "label == 'n' & (e_pred == True | c_pred == True)"
+    wrong_items_e = results.query(wrong_e)
+    wrong_items_c = results.query(wrong_c)
+    wrong_items_n = results.query(wrong_n)
+
+    return wrong_items_e, wrong_items_c, wrong_items_n 
