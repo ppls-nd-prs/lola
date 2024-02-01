1bd7952ede018288e70fa6e2aa911f4252bed570
BramSwaanen <bramswaanen@gmail.com>
2024-02-01 16:36:30 +0100

    Metrics test SICK

--- run_all.sh
+++ run_all.sh
@@ -2,8 +2,13 @@
 
 # Shell script for obtaining all evaluation and exceptions files
 
-for FILE in quantifier_ev plural_ev anaphora_ev ellipsis_ev adjectives_ev comparatives_ev temporal_ev verbs_ev attitudes_ev
+for FILE in sick_test_evaluation
 do
-    echo "Run with $FILE"
-    python get_conf_mats.py evaluations/*$FILE*
-done
+	for MOD in a2e i2c_a2e e_i2c e_i2c_split_adj split_verb split_adj a2e_i2c_split_adj
+	do
+		for LK in True False
+		do
+			python get_conf_mats.py mod_evaluations/[$MOD][$LK]$FILE.csv
+		done
+	done
+done	
