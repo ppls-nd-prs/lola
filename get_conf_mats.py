6092dc6182853ba0a2dd929aab86edfddedcd221
BramSwaanen <bramswaanen@gmail.com>
2024-02-09 12:11:18 +0100

    Conf mats verbeterd.

--- get_conf_mats.py
+++ get_conf_mats.py
@@ -8,7 +8,7 @@ file_path = args.file_path
 
 eval_df = pd.read_csv(file_path,sep='\t')
 
-table = {"classified as e":[],"classified as c":[],"classified as n":[],"classified as both":[]}
+table = {"classified as e":[],"classified as c":[],"classified as n":[]}
 
 # Fill class_as_e
 class_as_e = []
@@ -31,20 +31,24 @@ table['classified as c'] = class_as_c
 # Fill class_as_n
 class_as_n = []
 for i,gold in enumerate(['e','c','n']):
-    sub_df = eval_df[eval_df['label'] == gold]
-    sub_df = sub_df[sub_df['e_pred'] == False]
-    sub_df = sub_df[sub_df['c_pred'] == False]
-    class_as_n.append(len(sub_df))
+    sub_df1 = eval_df[eval_df['label'] == gold]
+    sub_df1 = sub_df1[sub_df1['e_pred'] == False]
+    sub_df1 = sub_df1[sub_df1['c_pred'] == False]
+    sub_df2 = eval_df[eval_df['label'] == gold]
+    sub_df2 = sub_df2[sub_df2['e_pred'] == True]
+    sub_df2 = sub_df2[sub_df2['c_pred'] == True]
+    class_as_n.append(len(sub_df1)+len(sub_df2))
 table['classified as n'] = class_as_n
 
-# Fill class_as_both
-class_as_both = []
-for i,gold in enumerate(['e','c','n']):
-    sub_df = eval_df[eval_df['label'] == gold]
-    sub_df = sub_df[sub_df['e_pred'] == True]
-    sub_df = sub_df[sub_df['c_pred'] == True]
-    class_as_both.append(len(sub_df))
-table['classified as both'] = class_as_both
+print(table)
+# # Fill class_as_both
+# class_as_both = []
+# for i,gold in enumerate(['e','c','n']):
+#     sub_df = eval_df[eval_df['label'] == gold]
+#     sub_df = sub_df[sub_df['e_pred'] == True]
+#     sub_df = sub_df[sub_df['c_pred'] == True]
+#     class_as_both.append(len(sub_df))
+# table['classified as n'] = class_as_both
 
 df = pd.DataFrame(table)
 df.to_csv(f'{file_path[:-4]}-[metrics].csv',sep='\t')
